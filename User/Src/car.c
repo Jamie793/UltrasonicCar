83bbe5bcdd3caeb1a6c9f2c1daca7b29bc0fb764
Jamie793 <doxm@foxmail.com>
2023-02-22 11:07:23 +0800

    2023-02-22 11:07

--- User/Src/car.c
+++ User/Src/car.c
@@ -2,50 +2,50 @@
 
 void Car_Turn_Left(void)
 {
-    MOTOR_A_CW;
+    MOTOR_A_CCW; 
     MOTOR_B_CW;
-    MOTOR_A_CW;
-    MOTOR_B_CW;
+    MOTOR_C_CCW;
+    MOTOR_D_CW;
 }
 
 void Car_Turn_Right(void)
 {
     MOTOR_A_CW;
-    MOTOR_B_CW;
-    MOTOR_A_CW;
-    MOTOR_B_CW;
+    MOTOR_B_CCW;
+    MOTOR_C_CW;
+    MOTOR_D_CCW;
 }
 
 void Car_Move_Forward(void)
 {
     MOTOR_A_CW;
     MOTOR_B_CW;
-    MOTOR_A_CW;
-    MOTOR_B_CW;
+    MOTOR_C_CW;
+    MOTOR_D_CW;
 }
 
 void Car_Move_Back(void)
 {
     MOTOR_A_CCW;
     MOTOR_B_CCW;
-    MOTOR_A_CCW;
-    MOTOR_B_CCW;
+    MOTOR_C_CCW;
+    MOTOR_D_CCW;
 }
 
 void Car_Move_Left(void)
 {
     MOTOR_A_CCW;
     MOTOR_B_CW; 
-    MOTOR_A_CCW;
-    MOTOR_B_CW;
+    MOTOR_C_CW;
+    MOTOR_D_CCW;
 }
 
 void Car_Move_Right(void)
 {
     MOTOR_A_CW;
     MOTOR_B_CCW;
-    MOTOR_A_CW;
-    MOTOR_B_CCW;
+    MOTOR_C_CCW;
+    MOTOR_D_CW;
 }
 
 void Car_PWM_Set(u32 speed)
